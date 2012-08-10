d7058112136224154a4956cb2b543aae15e83655
Lucas Serven <lserven@gmail.com>
2012-08-10 13:47:20 -0300

    Updated the regular expressions in rem.js.
    I made a small update to the regular expressions in rem.js. They were missing a feature that was stopping the code from making changes to rules preceeded, or followed, by a hash (#) symbol.

--- js/rem.js
+++ js/rem.js
@@ -47,7 +47,7 @@
     },
     
     matchcss = function ( response, i ) { //collect all of the rules from the xhr response texts and match them to a pattern
-        var pattern = /[\w\d\.\,\[\]\^\>\/<\+\~\|\-\_\$\#\"\'\/\*\\\=\s]+\{[\w\d\.\-\(\)\%\:\;\'\"\/\*\\\s]+\d*\.{0,1}\d+rem[\w\d\.\-\(\)\%\:\;\'\"\/\*\\\s]+\}/g; //find selectors that use rem in one or more of their rules
+        var pattern = /[\w\d\.\,\[\]\^\>\/<\+\~\|\-\_\$\#\"\'\/\*\\\=\s]+\{[\w\d\.\-\(\)\%\#\:\;\'\"\/\*\\\s]+\d*\.{0,1}\d+rem[\w\d\.\-\(\)\%\#\:\;\'\"\/\*\\\s]+\}/g; //find selectors that use rem in one or more of their rules
         var current = response.responseText.match(pattern);
         var remPattern =/\d*\.{0,1}\d+rem/g;
         var remCurrent = response.responseText.match(remPattern);
@@ -61,7 +61,7 @@
     },
 
     buildIt = function() { //first build each individual rule from elements in the found array and then add it to the string of rules.
-        var pattern = /[\w\d\.\-\(\)\%\:\'\"\/\*\\\s]+\d*\.{0,1}\d+rem[\w\d\.\-\(\)\%\:\'\"\/\*\\\s]*;/g; //find properties with rem values in them
+        var pattern = /[\w\d\.\-\(\)\%\#\:\'\"\/\*\\\s]+\d*\.{0,1}\d+rem[\w\d\.\-\(\)\%\#\:\'\"\/\*\\\s]*;/g; //find properties with rem values in them
         for( var i = 0; i < found.length; i++ ){
             rules = rules + found[i].substr(0,found[i].indexOf("{")+1); //save the selector portion of each rule with a rem value
             var current = found[i].match( pattern );
